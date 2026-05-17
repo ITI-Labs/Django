cadf565e89c8bab63bd48d7ee509ef4c94266210
Wafaey Khaled <wafaeykhaled732@gmail.com>
2026-05-18 01:20:20 +0300

    lab1

---
+++ market/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from market import views
+
+urlpatterns = [
+    path('', views.home, name='home_page'),
+    path('about/', views.about, name='about_page'),
+    path('contact/', views.contact, name='contact_page'),
+]
